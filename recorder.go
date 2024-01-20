2ecfccd70958e220a3717da2517c8c2025d4e8c4
Victor Denisov <denisovenator@gmail.com>
2024-01-19 21:32:54 -0800

    Add audio listener

--- recorder.go
+++ recorder.go
@@ -111,3 +111,87 @@ func record(fileName string, device string) {
 		}
 	}
 }
+
+type InputAudioStream struct {
+}
+
+func NewInputAudioStream(device string) *InputAudioStream {
+	var handle *C.snd_pcm_t
+	var rc C.int
+
+	deviceCString := C.CString(device)
+	defer C.free(unsafe.Pointer(deviceCString))
+
+	rc = C.snd_pcm_open(&handle, deviceCString, C.SND_PCM_STREAM_CAPTURE, 0)
+	if rc < 0 {
+		log.Fatal("Unable to open pcm device: %v", C.snd_strerror(rc))
+	}
+	defer C.snd_pcm_close(handle)
+	defer C.snd_pcm_drain(handle)
+
+	// Setup params
+	var params *C.snd_pcm_hw_params_t
+	rc = C.snd_pcm_hw_params_malloc(&params)
+	if rc < 0 {
+		log.Fatal("Couldn't alloc hw params")
+	}
+	defer C.snd_pcm_hw_params_free(params)
+	rc = C.snd_pcm_hw_params_any(handle, params)
+	if rc < 0 {
+		log.Fatal("Couldn't set default hw params")
+	}
+	rc = C.snd_pcm_hw_params_set_access(handle, params, C.SND_PCM_ACCESS_RW_INTERLEAVED)
+	if rc < 0 {
+		log.Fatal("Couldn't set access params")
+	}
+	rc = C.snd_pcm_hw_params_set_format(handle, params, C.SND_PCM_FORMAT_S16_LE)
+	if rc < 0 {
+		log.Fatal("Couldn't set endian format")
+	}
+	rc = C.snd_pcm_hw_params_set_channels(handle, params, 1)
+	if rc < 0 {
+		log.Fatal("Couldn't set channels")
+	}
+	var val C.uint = 44100
+	var dir C.int
+	rc = C.snd_pcm_hw_params_set_rate_near(handle, params, &val, &dir)
+	if rc < 0 {
+		log.Fatal("Couldn't set rate")
+	}
+
+	var frames C.snd_pcm_uframes_t = 32
+	rc = C.snd_pcm_hw_params_set_period_size_near(handle, params, &frames, &dir)
+	if rc < 0 {
+		log.Fatal("Couldn't set period size")
+	}
+
+	rc = C.snd_pcm_hw_params(handle, params)
+	if rc < 0 {
+		log.Fatal("Couldn't set params")
+	}
+
+	rc = C.snd_pcm_hw_params_get_period_size(params, &frames, &dir)
+	if rc < 0 {
+		log.Fatal("Couldn't get period size")
+	}
+
+	var size C.snd_pcm_uframes_t
+	log.Tracef("Frames: %v\n", frames)
+	size = frames * 2
+	buffer := make([]byte, size)
+	log.Tracef("Buffer len: %v", len(buffer))
+
+	rc = C.snd_pcm_hw_params_get_period_time(params, &val, &dir)
+	if rc < 0 {
+		log.Fatal("Couldn't get period time")
+	}
+
+	instance := &InputAudioStream{}
+
+	go instance.listen()
+
+	return instance
+}
+
+func (this *InputAudioStream) listen() {
+}
